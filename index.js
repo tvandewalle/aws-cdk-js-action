3269913842b9787b3036940844c414b7e06b58fb
Tim Van de Walle <tvandewalle@trek10.com>
2022-01-03 18:05:29 -0500

    First real commit

--- index.js
+++ index.js
@@ -7,9 +7,13 @@ async function run() {
   try {
     // Get inputs
     const cdkVersion = core.getInput('cdk_version');
+    const cdkCommand = core.getInput('cdk_command');
 
     // Install AWS CDK
     await exec.exec(`npm install -g aws-cdk@${cdkVersion}`);
+
+    // Run Command
+    await exec.exec(`cdk ${cdkCommand}`);
 
     // Set CDK CLI Output
     core.setOutput('status_code', '0');

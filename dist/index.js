64e91ad8ed730328a5f83bd50f8747dec024420a
Tim Van de Walle <tvandewalle@trek10.com>
2022-01-03 18:13:56 -0500

    Add dist/

--- dist/index.js
+++ dist/index.js
@@ -2976,9 +2976,27 @@ async function run() {
   try {
     // Get inputs
     const cdkVersion = core.getInput('cdk_version');
+    const cdkCommand = core.getInput('cdk_command');
 
     // Install AWS CDK
     await exec.exec(`npm install -g aws-cdk@${cdkVersion}`);
+
+    // Run Command
+    let commandOut = '';
+    let commandErr = '';
+
+    const options = {};
+    options.listeners = {
+      stdout: (data) => {
+        commandOut += data.toString();
+      },
+      stderr: (data) => {
+        commandErr += data.toString();
+      }
+    };
+    await exec.exec('cdk', [cdkCommand], options);
+    console.log(`Command Output: ${commandOut}`);
+    console.log(`Command Error: ${commandErr}`)
 
     // Set CDK CLI Output
     core.setOutput('status_code', '0');
